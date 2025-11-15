d8a8701dd8772340d89a4b3e5e344434697782d4
Matus Fialka <matus1fialka@gmail.com>
2025-11-15 13:04:38 +0100

    Inicializacia working version

--- Classes/User.ts
+++ Classes/User.ts
@@ -1,22 +1,21 @@
 import { Registracia } from "./Registracia";
 
 export class User {
-    private meno: string;
-    private heslo: string;
     private registracia: Registracia;
 
     constructor(registracia: Registracia) {
-        this.meno = registracia.celeMeno;
-        this.heslo = registracia.heslo;
         this.registracia = registracia;
     }
 
     prihlasSa(zadaneMeno: string, zadaneHeslo: string): void {
-        if (zadaneMeno === this.meno && zadaneHeslo === this.heslo) {
-            console.log(`✅ Prihlásenie úspešné. Vitaj, ${this.meno}!`);
+        if (zadaneMeno === this.registracia.celeMeno && zadaneHeslo === this.registracia.heslo)
+        {
+            console.log(`Prihlásenie úspešné. Vitaj, ${this.registracia.celeMeno}!`);
             this.registracia.vypisInfo();
-        } else {
-            console.log("❌ Nesprávne meno alebo heslo. Prihlásenie zlyhalo.");
+        }
+        else
+        {
+            console.log("Nesprávne meno alebo heslo. Prihlásenie zlyhalo.");
         }
     }
 }
--- index.ts
+++ index.ts
@@ -1,6 +1,22 @@
 import { Registracia } from "./Classes/Registracia";
 import { User } from "./Classes/User";
+import express = require('express')
+import {Request, Response} from "express";
 
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
+//-------------------------------------------
 // Registrácia používateľa (napr. po registrácii v systéme)
 const registrovany = new Registracia(
     "Ján Fialka",
